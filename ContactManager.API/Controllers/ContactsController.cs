e8879ab7c1662c4b2202281149a4cf0235de0519
Jeremy Jules Flores <jeremyjules.flores@cit.edu>
2023-07-26 12:16:27 +0800

    Created Contacts Controller

---
+++ ContactManager.API/Controllers/ContactsController.cs
@@ -0,0 +1,30 @@
+﻿using ContactManager.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/contacts")]
+    public class ContactsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ContactDto>> GetContacts()
+        {
+            return Ok(ContactsDataStore.Current.Contacts);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ContactDto> GetContact(int id)
+        {
+            var contactToReturn = ContactsDataStore.Current.Contacts
+                .FirstOrDefault(c => c.Id == id);
+
+            if(contactToReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(contactToReturn);
+        }
+  
+    }
+}
