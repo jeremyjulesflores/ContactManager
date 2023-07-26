1e88389a1326c138c79752734deaa5c4263a2f09
Jeremy Jules Flores <jeremyjules.flores@cit.edu>
2023-07-26 10:40:30 +0800

    Add Address Entity

---
+++ ContactManager.API/Entities/Address.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactManager.API.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("ContactId")]
+        public Contact Contact { get; set; }
+        public int ContactId { get; set; }
+
+        [Required]
+        public AddressType Type { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string AddressDetails { get; set; }
+
+    }
+
+    public enum AddressType
+    {
+        Billing,
+        Delivery,
+        Home,
+        Work
+    }
+}
