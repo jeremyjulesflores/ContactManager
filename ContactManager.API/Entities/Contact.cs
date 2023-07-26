076cfe441dea22f789c85b47371ca820ec06f7d3
Jeremy Jules Flores <jeremyjules.flores@cit.edu>
2023-07-26 10:27:02 +0800

    Add Contact Entity

---
+++ ContactManager.API/Entities/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.API.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        public bool Favorite { get; set; }
+        public bool Emergency { get; set; }
+        [MaxLength(200)]
+        public string? Note { get; set; }
+    }
+}
