b822e289d1dc59e77de8a97ee612fade0777cfff
Jeremy Jules Flores <jeremyjules.flores@cit.edu>
2023-07-27 08:26:25 +0800

    Db Migrations

---
+++ ContactManager.API/Migrations/20230726234200_ContactInfoDbInitialMigration.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManager.API.Migrations
+{
+    public partial class ContactInfoDbInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Favorite = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Emergency = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Note = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ContactId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", maxLength: 30, nullable: false),
+                    AddressDetails = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Address_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Email",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ContactId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", maxLength: 30, nullable: false),
+                    EmailAddress = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Email", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Email_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Number",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ContactId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", maxLength: 30, nullable: false),
+                    ContactNumber = table.Column<string>(type: "TEXT", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Number", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Number_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_ContactId",
+                table: "Address",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Email_ContactId",
+                table: "Email",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Number_ContactId",
+                table: "Number",
+                column: "ContactId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Email");
+
+            migrationBuilder.DropTable(
+                name: "Number");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ ContactManager.API/Migrations/20230727000926_DataSeed.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManager.API.Migrations
+{
+    public partial class DataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Emergency", "Favorite", "FirstName", "LastName", "Note" },
+                values: new object[] { 1, false, false, "Jeremy", "Flores", "This is a Note" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Emergency", "Favorite", "FirstName", "LastName", "Note" },
+                values: new object[] { 2, false, false, "Lirae", "Data", "This is a Note 2" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Emergency", "Favorite", "FirstName", "LastName", "Note" },
+                values: new object[] { 3, false, false, "Charis Arlie", "Baclayon", null });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "AddressDetails", "ContactId", "Type" },
+                values: new object[] { 1, "This is my address 1", 1, "Home" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "AddressDetails", "ContactId", "Type" },
+                values: new object[] { 2, "This is Fullscale Address", 1, "Work" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "AddressDetails", "ContactId", "Type" },
+                values: new object[] { 3, "This is my address", 2, "Home" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "AddressDetails", "ContactId", "Type" },
+                values: new object[] { 4, "This is Fullscale Address", 3, "Work" });
+
+            migrationBuilder.InsertData(
+                table: "Email",
+                columns: new[] { "Id", "ContactId", "EmailAddress", "Type" },
+                values: new object[] { 1, 3, "jeremygwapo@gmail.com", "Home" });
+
+            migrationBuilder.InsertData(
+                table: "Number",
+                columns: new[] { "Id", "ContactId", "ContactNumber", "Type" },
+                values: new object[] { 1, 1, "099292929", "Work" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Address",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Address",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Address",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Address",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Email",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Number",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
