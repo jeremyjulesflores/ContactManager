e1ae5483c1717d3638e9e8668414ab50857d5309
Jeremy Jules Flores <jeremyjulesflores1@gmail.com>
2023-07-31 21:15:33 +0800

    Implement Create and Get Number in NumbersController

--- ContactManager.API/Controllers/NumbersController.cs
+++ ContactManager.API/Controllers/NumbersController.cs
@@ -1,4 +1,6 @@
 ﻿using ContactManager.API.Models;
+using ContactManager.API.Models.CreationDtos;
+using ContactManager.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,37 +10,64 @@ namespace ContactManager.API.Controllers
     [ApiController]
     public class NumbersController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<NumberDto>> GetNumbers(int contactId)
-        {
-            var contact = ContactsDataStore.Current.Contacts.FirstOrDefault(c => c.Id == contactId);
+        private readonly INumberService _numberService;
+        private readonly ILogger<NumbersController> _logger;
 
-            if(contact == null)
+        public NumbersController(INumberService numberService,
+                                 ILogger<NumbersController> logger)
+        {
+            this._numberService = numberService;
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NumberDto>>> GetNumbersAsync(int contactId)
+        {
+            try
             {
-                return NotFound();
+                var numbers = await _numberService.GetNumbers(contactId);
+
+                if(numbers == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(numbers);
             }
-            return Ok(contact.Numbers);
+            catch(Exception ex)
+            {
+                _logger.LogCritical(
+                    $"Exception while getting Numbers for contact with id {contactId}.", ex);
+                return StatusCode(500, "Something went wrong");
+            }
         }
 
         [HttpGet("{numberId}")]
-        public ActionResult<NumberDto> GetAddress(int contactId,
-                                                   int numberId)
+        public async Task<ActionResult<AddressDto>> GetNumberAsync(int contactId,
+                                                                    int numberId)
         {
-            var contact = ContactsDataStore.Current.Contacts.FirstOrDefault(c => c.Id == contactId);
+            var number = await _numberService.GetNumber(contactId, numberId);
 
-            if(contact == null)
-            {
-                return NotFound();
-            }
-
-            var number = contact.Numbers.FirstOrDefault(c => c.Id == numberId);
-
-            if(number == null)
+            if (number == null)
             {
                 return NotFound();
             }
 
             return Ok(number);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<NumberDto>> CreateNumberAsync(int contactId,
+                                                      NumberCreationDto number)
+        {
+            var created = await this._numberService.CreateNumber(contactId, number);
+
+            if (!created)
+            {
+                return BadRequest();
+            }
+
+            return Ok("Number Successfully Created");
         }
     }
 }
