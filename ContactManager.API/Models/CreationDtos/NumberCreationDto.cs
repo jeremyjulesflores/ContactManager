e88ce698d1cfa2ae70b46c673cf1dbd961821caf
Jeremy Jules Flores <jeremyjulesflores1@gmail.com>
2023-07-31 21:15:13 +0800

    Create Number Dtos

---
+++ ContactManager.API/Models/CreationDtos/NumberCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.API.Models.CreationDtos
+{
+    public class NumberCreationDto
+    {
+        [Required(ErrorMessage = "Type is Required")]
+        [MaxLength(50)]
+        public string Type { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Contact Number is Required")]
+        [MaxLength(50)]
+        public string ContactNumber { get; set; } = string.Empty;
+    }
+}
